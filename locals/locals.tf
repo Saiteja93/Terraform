7e6ea5cdda70963757be9e304278edbd0eabb4c4
Saiteja93 <saitejaraj.guvvala@gmail.com>
2025-04-05 12:07:19 -0700

    locals zone id

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,6 +1,6 @@
 locals {
     domain_name  = "saiteja-guvva.online"
-    zone_id = "Z09912121MS725XSKH1TG"
+    zone_id = "Z082474033B2E28EW4DD4"
     instance_type = var.environment == "prod" ? "t3.medium" : "t3.micro"
     # count.index will not work in locals
 }
